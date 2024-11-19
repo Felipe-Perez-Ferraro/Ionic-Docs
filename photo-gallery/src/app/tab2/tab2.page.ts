387fbfb10624d573d890340a00fbc301141c2b79
Felipe <fperezferraro@cdt.com.ar>
2024-11-19 13:10:16 -0300

    Inicializo 01-my-app

--- photo-gallery/src/app/tab2/tab2.page.ts
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { UserPhoto } from '../interfaces/user-photo.interface';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  private photoService = inject(PhotoService);
 
-  constructor() {}
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 
+  userPhotos(): UserPhoto[] {
+    return this.photoService.userPhotosArr;
+  }
 }
