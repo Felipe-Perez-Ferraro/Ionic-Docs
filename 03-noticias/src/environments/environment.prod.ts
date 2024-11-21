9c291ca7b8daf9591d71bc1fdb6882657d10371a
Felipe <fperezferraro@cdt.com.ar>
2024-11-21 18:19:32 -0300

    Ignoro la carpeta de environments

--- 03-noticias/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: '9b66364214364c82bcbe15efbfaa8f14',
-};
--- 03-noticias/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiKey: '9b66364214364c82bcbe15efbfaa8f14',
-};
