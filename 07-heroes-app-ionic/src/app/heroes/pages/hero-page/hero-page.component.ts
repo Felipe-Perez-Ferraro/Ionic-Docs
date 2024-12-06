3b866536f1007740ba334548889f69289f5501d3
Felipe <fperezferraro@cdt.com.ar>
2024-12-06 16:23:42 -0300

    Ultimos toques

--- 07-heroes-app-ionic/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ 07-heroes-app-ionic/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero-response.interface';
+import { HeroesService } from '../../services/hero.service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styleUrls: ['./hero-page.component.scss'],
 })
-export class HeroPageComponent  implements OnInit {
+export class HeroPageComponent implements OnInit {
+  hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((res) => (this.hero = res));
+  }
 }
